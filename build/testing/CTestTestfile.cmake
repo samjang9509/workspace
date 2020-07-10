727119feacfd05053ddceeec61d97562368e7994
samjang9509 <samjang9509@github.com>
2020-07-10 11:35:08 +0900

    work

---
+++ build/testing/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/samuel/workspace/src/testing
+# Build directory: /home/samuel/workspace/build/testing
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
